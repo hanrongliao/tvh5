cf58558bdfd24d742c1338f5ab8afe2e4ad3e7e4
hr <hanrongliao@email.com>
2020-12-01 16:36:33 +0800

    完成

---
+++ app/src/main/java/com/unicom/myapplication/MainActivity.java
@@ -0,0 +1,24 @@
+package com.unicom.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView myWebView = (WebView) findViewById(R.id.webview);
+        myWebView.clearCache(false);
+        WebSettings webSettings = myWebView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        myWebView.loadUrl("http://www.baidu.com");
+
+    }
+}
