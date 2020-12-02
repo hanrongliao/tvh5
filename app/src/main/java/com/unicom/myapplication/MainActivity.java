8f0e103811940c94848ec1c55b73c4f7db2be926
addi <qidi@m.scnu.edu.cn>
2020-12-02 15:24:18 +0800

    fix:禁用重定向

--- app/src/main/java/com/unicom/myapplication/MainActivity.java
+++ app/src/main/java/com/unicom/myapplication/MainActivity.java
@@ -1,10 +1,13 @@
 package com.unicom.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.webkit.WebChromeClient;
+import android.webkit.WebResourceRequest;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,11 +17,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         WebView myWebView = (WebView) findViewById(R.id.webview);
+        //禁止重定向
+        myWebView.setWebViewClient(new WebViewClient(){
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
+                return false;
+            }
+        });
         myWebView.clearCache(false);
         WebSettings webSettings = myWebView.getSettings();
+        webSettings.setUserAgentString("PC");
         webSettings.setJavaScriptEnabled(true);
 
-        myWebView.loadUrl("http://www.baidu.com");
+        myWebView.loadUrl("https://www.baidu.com/");
 
     }
 }
